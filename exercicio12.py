3862b7a98716846550133029ab84b9adc8355ea7
CCoxinho <coxynho@hotmail.com>
2014-11-21 19:57:43 +0000

    Project Euler

---
+++ exercicio12.py
@@ -0,0 +1,24 @@
+import math
+
+def highly_divisible():
+    l = []
+    one = 0
+    a = 1
+    b = 2
+    while one == 0:
+        a = a + b 
+        b += 1
+        l = []
+
+        sqrt_a = int(math.sqrt(a))
+
+        for x in range(1, sqrt_a + 1):
+            if a % x == 0:
+                l.append(x)
+                if x < math.sqrt(a):
+                    l.append(a / x)
+                if len(l) > 500:
+                    one = 1
+
+    print(a)
+
